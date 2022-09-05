d4ad1e8ed6765a28f39b632935e1c1b95bbd80b9
OlivierCoq <coq.olivier36@gmail.com>
2022-09-05 19:30:39 +0000

    updating db config

--- config/database.js
+++ config/database.js
@@ -6,10 +6,19 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
     },
-    useNullAsDefault: true,
+    options: {
+      pool:{
+          min: 0,
+          max: 10,
+          idleTimeoutMillis: 30000,
+          createTimeoutMillis: 30000,
+          acquireTimeoutMillis: 30000
+      }
+  },
+  useNullAsDefault: true,
   },
 });
