b62bcf91802caf81aed9744e17be067e33228551
OlivierC <coq.olivier36@gmail.com>
2022-11-06 23:17:25 -0500

    Updating email api key

--- config/plugins.js
+++ config/plugins.js
@@ -18,7 +18,7 @@ module.exports = ({ env }) => ({
       config: {
         provider: 'sendgrid', 
         providerOptions: {
-          apiKey: env('nsightvip_email_api_key'),
+          apiKey: env('nsight_api_email_key'),
         },
         settings: {
           defaultFrom: 'info@nsight.online',
