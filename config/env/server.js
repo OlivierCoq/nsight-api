a3e4f5a4074889865156f0c71bbe6134e6169ac1
OlivierC <coq.olivier36@gmail.com>
2022-11-10 23:39:05 -0500

    Adjusting config files

---
+++ config/env/server.js
@@ -0,0 +1,5 @@
+module.exports = ({ env }) => ({
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT'),
+    url: env('BACKEND_URL'),
+}); 
--- config/server.js
+++ config/server.js
@@ -6,3 +6,4 @@ module.exports = ({ env }) => ({
     keys: env.array('APP_KEYS'),
   },
 });
+ 
